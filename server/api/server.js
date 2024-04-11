d751a1cc3902f378fe0678494f191d60550a49b0
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-04-11 15:30:15 -0300

    fix: Ajustando instalação do json-server V

--- server/api/server.js
+++ server/api/server.js
@@ -2,7 +2,12 @@ const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
-const router = jsonServer.router('tmp/db.json')
+const path = require('path')
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
+
+// const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
