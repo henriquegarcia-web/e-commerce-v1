82ce47a7f6dbbdf732cdf3a67ad8cfd729e2ebcb
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-04-11 16:51:45 -0300

    chore: Ajustando variáveis e rotas do back-end e arquivos package.json

--- server/api/server.js
+++ server/api/server.js
@@ -1,19 +1,18 @@
 const jsonServer = require('json-server')
-
-const server = jsonServer.create()
-
 const path = require('path')
 const fs = require('fs')
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
-const router = jsonServer.router(db)
 
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+
+const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
+const router = jsonServer.router(db)
 
 server.use(middlewares)
 server.use(router)
 
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+server.listen(5000, () => {
+  console.log('JSON Server rodando')
 })
 
 module.exports = server
